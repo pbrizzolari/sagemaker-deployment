f7a35f5aec52b643c98c6e17cfd5a96a8c7a8be8
Paolo Brizzolari <pbrizzolari@gmail.com>
2020-12-03 12:30:52 +0000

    Work In Progress.

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -87,8 +87,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-    with torch.no_grad():
-        output = model.forward(data)
-    result = int(np.round(output.to('cpu').numpy()))
+    output = model.forward(data)
+    result = int(np.round(output.to('cpu').detach().numpy()))
 
     return result
